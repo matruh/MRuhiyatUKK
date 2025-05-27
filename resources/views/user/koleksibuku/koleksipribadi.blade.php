c5664e1ee928267d5c0bdbb9f85711b005218078
muhammad ruhiyat <ruhiyatmuhammad0@gmail.com>
2025-05-27 10:29:03 +0700

    coba css

--- resources/views/user/koleksibuku/koleksipribadi.blade.php
+++ resources/views/user/koleksibuku/koleksipribadi.blade.php
@@ -10,7 +10,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- Toastr CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    <link rel="stylesheet" href="{{asset('assets/css/sidebar.css')}}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/sidebar.css') }}">
+
 
 </head>
 
--- resources/views/user/ulasan/ulasanbuku.blade.php
+++ resources/views/user/ulasan/ulasanbuku.blade.php
@@ -8,7 +8,8 @@
     <!-- Sertakan CSS Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    <link rel="stylesheet" href="{{asset('assets/css/sidebar.css')}}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/sidebar.css') }}">
+
 
 </head>
 
